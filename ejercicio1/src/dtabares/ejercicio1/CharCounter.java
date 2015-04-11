ea4c088bedd1f17dc798e3a424638e975ea7ed2d
Diego Tabares <dtabares@gmail.com>
2015-04-11 11:08:44 -0300

    resuelvo el primer test del ejercicio 1

---
+++ ejercicio1/src/dtabares/ejercicio1/CharCounter.java
@@ -0,0 +1,27 @@
+package dtabares.ejercicio1;
+
+public class CharCounter  {
+
+	private int stringToAnalizeLenght;
+	private int ocurrences;
+	private String stringToAnalize;
+
+	public CharCounter(String stringToAnalize) {
+		this.stringToAnalize = stringToAnalize;
+		this.ocurrences = 0;
+	}
+	
+	public int howMany (char charValue){
+
+		stringToAnalizeLenght = stringToAnalize.length();
+
+		for (int i = 0; i < stringToAnalizeLenght; i++) {
+
+			if(this.stringToAnalize.charAt(i) == charValue){
+				this.ocurrences++;
+			}
+		}
+
+		return this.ocurrences;
+	}
+}
