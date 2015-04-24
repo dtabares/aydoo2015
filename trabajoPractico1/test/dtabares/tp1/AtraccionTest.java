f3ae1f9a7c7a4c570fda2f66a672104a7f941d2f
Diego Tabares <dtabares@gmail.com>
2015-04-24 14:29:25 -0300

    agrego test a AtraccionTest

--- trabajoPractico1/test/dtabares/tp1/AtraccionTest.java
+++ trabajoPractico1/test/dtabares/tp1/AtraccionTest.java
@@ -29,6 +29,19 @@ public class AtraccionTest {
         Posicion posicion = new Posicion(-34.6037232,-58.3815931 );
         Atraccion atraccionDePrueba = new Atraccion (nombre, costo, duracionPromedioDeVisitaEnMins, cupoDeVisitantesDiarios, tipoDeAtraccion, posicion);
 
-        Assert.assertEquals("La Casa de Bilbo",atraccionDePrueba.getNombre());
+        Assert.assertEquals("La Casa de Bilbo",atraccionDePrueba.obtenerNombre());
+    }
+
+    @Test
+    public void elTipoDeAtraccionDebeSerAventura(){
+        String nombre = "La Casa de Bilbo";
+        double costo = 200;
+        int duracionPromedioDeVisitaEnMins = 20;
+        int cupoDeVisitantesDiarios = 500;
+        TipoDeAtraccion tipoDeAtraccion = TipoDeAtraccion.Aventura;
+        Posicion posicion = new Posicion(-34.6037232,-58.3815931 );
+        Atraccion atraccionDePrueba = new Atraccion (nombre, costo, duracionPromedioDeVisitaEnMins, cupoDeVisitantesDiarios, tipoDeAtraccion, posicion);
+
+        Assert.assertEquals(TipoDeAtraccion.Aventura,atraccionDePrueba.obtenerTipoDeAtraccion());
     }
 }
