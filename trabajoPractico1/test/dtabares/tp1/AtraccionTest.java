10bede57e7df851721fb70070ba69f9124751962
Diego Tabares <dtabares@gmail.com>
2015-04-26 20:03:01 -0300

    agrego tests cases

--- trabajoPractico1/test/dtabares/tp1/AtraccionTest.java
+++ trabajoPractico1/test/dtabares/tp1/AtraccionTest.java
@@ -44,4 +44,36 @@ public class AtraccionTest {
 
         Assert.assertEquals(TipoDeAtraccion.Aventura,atraccionDePrueba.obtenerTipoDeAtraccion());
     }
+
+    @Test
+    public void compararDosAtraccionesConElMismoNombreDebeDevolverCero(){
+        String nombre = "La Casa de Bilbo";
+        double costo = 200;
+        int duracionPromedioDeVisitaEnMins = 20;
+        int cupoDeVisitantesDiarios = 500;
+        TipoDeAtraccion tipoDeAtraccion = TipoDeAtraccion.Aventura;
+        Posicion posicion = new Posicion(-34.6037232,-58.3815931 );
+        Atraccion atraccionDePrueba = new Atraccion (nombre, costo, duracionPromedioDeVisitaEnMins, cupoDeVisitantesDiarios, tipoDeAtraccion, posicion);
+        Atraccion otraAtraccion = new Atraccion (nombre, 300, duracionPromedioDeVisitaEnMins, cupoDeVisitantesDiarios, tipoDeAtraccion, posicion);
+
+        Assert.assertEquals(0,atraccionDePrueba.compareTo(otraAtraccion));
+    }
+
+
+    @Test
+    public void compararDosAtraccionesConDistintoNombreDebeDevolverDistintoDeCero(){
+
+        String nombre = "La Casa de Bilbo";
+        double costo = 200;
+        int duracionPromedioDeVisitaEnMins = 20;
+        int cupoDeVisitantesDiarios = 500;
+        TipoDeAtraccion tipoDeAtraccion = TipoDeAtraccion.Aventura;
+        Posicion posicion = new Posicion(-34.6037232,-58.3815931 );
+        Atraccion atraccionDePrueba = new Atraccion (nombre, costo, duracionPromedioDeVisitaEnMins, cupoDeVisitantesDiarios, tipoDeAtraccion, posicion);
+        Atraccion otraAtraccion = new Atraccion ("Mordor", costo, duracionPromedioDeVisitaEnMins, cupoDeVisitantesDiarios, tipoDeAtraccion, posicion);
+
+        Assert.assertEquals(-1,atraccionDePrueba.compareTo(otraAtraccion));
+
+    }
+
 }
