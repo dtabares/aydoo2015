780a86ee5fcbc0a172030531f0b5a02d56afb09b
Diego Tabares <dtabares@gmail.com>
2015-04-27 00:15:13 -0300

    Agrego la clase AxB y sus respectivos Test

---
+++ trabajoPractico1/src/dtabares/tp1/AxB.java
@@ -0,0 +1,31 @@
+package dtabares.tp1;
+
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class AxB extends Promocion{
+
+
+
+    private Atraccion atraccionQueSeDescuentaCostoEnSuTotalidad;
+
+    public AxB(String nombre, PeriodoDeVigencia periodoDeVigencia,Set atracciocionesSujetasADescuento, Atraccion atraccionQueSeDescuentaCostoEnSuTotalidad){
+        this.nombre = nombre;
+        this.periodoDeVigencia = periodoDeVigencia;
+        this.atracciocionesSujetasADescuento = atracciocionesSujetasADescuento;
+        this.atraccionQueSeDescuentaCostoEnSuTotalidad = atraccionQueSeDescuentaCostoEnSuTotalidad;
+        this.reduccionDeCostoTotal = 0;
+    }
+
+    @Override
+    public double calcularReduccionDeCostoTotal(Set atraccionesQueElTuristaVisitara) {
+
+        if(atraccionesQueElTuristaVisitara.containsAll(this.atracciocionesSujetasADescuento) && atraccionesQueElTuristaVisitara.contains(atraccionQueSeDescuentaCostoEnSuTotalidad) && ! this.atracciocionesSujetasADescuento.isEmpty()) {
+
+            this.reduccionDeCostoTotal = atraccionQueSeDescuentaCostoEnSuTotalidad.obtenerCosto();
+        }
+
+        return this.reduccionDeCostoTotal;
+    }
+}
