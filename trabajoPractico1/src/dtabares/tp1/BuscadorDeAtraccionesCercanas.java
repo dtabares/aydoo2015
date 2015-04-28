2163e83ade1c26c90bb08701f2c68504340f7033
Diego Tabares <dtabares@gmail.com>
2015-04-28 07:39:03 -0300

    Agrego Clase BuscadorDeAtraccionesCercanas y sus respectivos tests

---
+++ trabajoPractico1/src/dtabares/tp1/BuscadorDeAtraccionesCercanas.java
@@ -0,0 +1,68 @@
+package dtabares.tp1;
+
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class BuscadorDeAtraccionesCercanas {
+
+
+    //Devuelve un Set De Atracciones ya que puede que 2 o mas esten a la misma distancia
+    public static Set<Atraccion> buscarAtraccionMasCercana(Posicion posicionActual,Set<Atraccion> atraccionesDisponibles){
+
+        Set<Atraccion> atraccionesMasCercanas = new HashSet();
+        Atraccion atracciónConLaDistanciaMasBajaEncontrada;
+        Iterator iterador = atraccionesDisponibles.iterator();
+        double distanciaAlaAtraccionMasCercana;
+        double distanciaDeLaAtraccionAComparar;
+
+
+        if(iterador.hasNext()){
+            //Por ser la primera, es la mas cercana.
+            atracciónConLaDistanciaMasBajaEncontrada = (Atraccion) iterador.next();
+            distanciaAlaAtraccionMasCercana = CalculadorDeDistancia.calcularDistanciaEntreDosPosiciones(posicionActual,atracciónConLaDistanciaMasBajaEncontrada.obtenerPosicion());
+            atraccionesMasCercanas.add(atracciónConLaDistanciaMasBajaEncontrada);
+
+            while(iterador.hasNext()){
+                Atraccion atraccionAComparar  = (Atraccion) iterador.next();
+                distanciaDeLaAtraccionAComparar = CalculadorDeDistancia.calcularDistanciaEntreDosPosiciones(posicionActual,atraccionAComparar.obtenerPosicion());
+
+                System.out.println("Atraccion Con La Dist Mas Baja: " + atracciónConLaDistanciaMasBajaEncontrada.obtenerNombre());
+                System.out.println("Atraccion a Comparar: " + atraccionAComparar.obtenerNombre());
+                System.out.println("distanciaAlaAtraccionMasCercana: " + distanciaAlaAtraccionMasCercana);
+                System.out.println("distanciaDeLaAtraccionAComparar: " + distanciaDeLaAtraccionAComparar);
+                //Si esto es True quiere decir que el usuario estaba parado en esta atraccion
+                if (distanciaAlaAtraccionMasCercana == 0){
+                    System.out.println("Entre");
+                    atraccionesMasCercanas.clear();
+                    atracciónConLaDistanciaMasBajaEncontrada = atraccionAComparar;
+                    atraccionesMasCercanas.add(atracciónConLaDistanciaMasBajaEncontrada);
+
+                }
+                else{
+
+                    if(distanciaDeLaAtraccionAComparar < distanciaAlaAtraccionMasCercana && distanciaDeLaAtraccionAComparar != 0){
+
+                        atraccionesMasCercanas.clear();
+                        atracciónConLaDistanciaMasBajaEncontrada = atraccionAComparar;
+                        atraccionesMasCercanas.add(atracciónConLaDistanciaMasBajaEncontrada);
+                    }
+                    else {
+                        if (distanciaDeLaAtraccionAComparar == distanciaAlaAtraccionMasCercana){
+                            atraccionesMasCercanas.add(atraccionAComparar);
+                        }
+                    }
+
+                }
+
+
+
+            }
+        }
+
+
+        return atraccionesMasCercanas;
+
+    }
+}
